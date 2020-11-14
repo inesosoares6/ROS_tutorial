232eb5ea4a4e0876b54afce97ba4a83a8179a2a2
Ines Soares <up201606615@fe.up.pt>
2020-11-14 20:27:25 +0000

    done (i guess)

---
+++ src/husky_highlevel_controller/build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/mirrorlabs/eth_ws/src/husky_highlevel_controller/include".split(';') if "/home/mirrorlabs/eth_ws/src/husky_highlevel_controller/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;sensor_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "husky_highlevel_controller"
+PROJECT_SPACE_DIR = "/home/mirrorlabs/eth_ws/src/husky_highlevel_controller/build/devel"
+PROJECT_VERSION = "0.1.0"
--- src/husky_highlevel_controller/src/HuskyHighlevelController.cpp
+++ src/husky_highlevel_controller/src/HuskyHighlevelController.cpp
@@ -10,8 +10,7 @@ HuskyHighlevelController::HuskyHighlevelController(ros::NodeHandle& nodeHandle) 
   ROS_INFO_STREAM(topic_name << " " << queue_size);
 	laser_scan_subs = nodeHandle.subscribe(topic_name, queue_size, &HuskyHighlevelController::laser_scan_Callback, this); 
 }
-
-void HuskyHighLevelController::laser_scan_Callback(const sensor_msgs::LaserScan::ConstPtr& laser_scan_msgs){
+void HuskyHighlevelController::laser_scan_Callback(const sensor_msgs::LaserScan::ConstPtr& laser_scan_msgs){
   minDistance = 30;
   direction_index = 0;
   laser_scan_distance=laser_scan_msgs->ranges;
@@ -24,10 +23,12 @@ void HuskyHighLevelController::laser_scan_Callback(const sensor_msgs::LaserScan:
   }
   ROS_INFO_STREAM("The " << direction_index << "st message is chosen in " << laser_scan_distance.size() << " messages");
   ROS_INFO_STREAM("minDistance detected by laser: " << minDistance);
+
 }
 
 HuskyHighlevelController::~HuskyHighlevelController()
 {
 }
+
 
 } /* namespace */
